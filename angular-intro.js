f5af8d9dc88c8392f47fb19fc29b4816b973c259
Moritz Meyer <m0ritz.m3yer@gmail.com>
2014-03-31 14:56:33 +0200

    Using $parse to evaluate the incoming attributes

--- angular-intro.js
+++ angular-intro.js
@@ -1,60 +1,58 @@
-var ngIntroDirective = angular.module('angular-intro',[]);
+var ngIntroDirective = angular.module('angular-intro', []);
 
-// TODO: Use isolate scope, but requires angular 1.2: http://plnkr.co/edit/a2c14O?p=preview
-// See: http://stackoverflow.com/questions/18796023/in-a-directive-handle-calls-to-a-user-defined-method-name
+/**
+ * TODO: Use isolate scope, but requires angular 1.2: http://plnkr.co/edit/a2c14O?p=preview
+ * See: http://stackoverflow.com/q/18796023/237209
+ */
 
-ngIntroDirective.directive('ngIntroOptions', ['$timeout', function ($timeout) {
+ngIntroDirective.directive('ngIntroOptions', ['$timeout', '$parse', function ($timeout, $parse) {
 
-   return {
-       restrict: 'A',
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
 
-       link: function(scope, element, attrs){
+            scope[attrs.ngIntroMethod] = function(step) {
 
-           scope[attrs.ngIntroMethod] = function(step) {
+                if(typeof(step) === 'string') {
+                    var intro = introJs(step);
+                } else {
+                    var intro = introJs();
+                }
 
-               if(typeof(step)=="string"){
-                   var intro = introJs(step);
-               }
-               else{
-                   var intro = introJs();
-               }
+                intro.setOptions(scope.$eval(attrs.ngIntroOptions));
 
-               intro.setOptions(scope.$eval(attrs.ngIntroOptions));
+                if(attrs.ngIntroOncomplete) {
+                    intro.oncomplete($parse(attrs.ngIntroOncomplete)(scope));
+                }
 
-               if(attrs.ngIntroOncomplete){
-                   intro.oncomplete(scope[attrs.ngIntroOncomplete]);
-               }
+                if(attrs.ngIntroOnexit) {
+                    intro.onexit($parse(attrs.ngIntroOnexit)(scope));
+                }
 
-               if(attrs.ngIntroOnexit){
-                   intro.onexit(scope[attrs.ngIntroOnexit]);
-               }
+                if(attrs.ngIntroOnchange) {
+                    intro.onchange($parse(attrs.ngIntroOnchange)(scope));
+                }
 
-               if(attrs.ngIntroOnchange){
-                   intro.onchange(scope[attrs.ngIntroOnchange]);
-               }
+                if(attrs.ngIntroOnbeforechange) {
+                    intro.onbeforechange($parse(attrs.ngIntroOnbeforechange)(scope));
+                }
 
-               if(attrs.ngIntroOnbeforechange){
-                   intro.onbeforechange(scope[attrs.ngIntroOnbeforechange]);
-               }
+                if(attrs.ngIntroOnafterchange) {
+                    intro.onafterchange($parse(attrs.ngIntroOnafterchange)(scope));
+                }
 
-               if(attrs.ngIntroOnafterchange){
-                   intro.onafterchange(scope[attrs.ngIntroOnafterchange]);
-               }
+                if(typeof(step) === 'number') {
+                    intro.goToStep(step).start();
+                } else {
+                    intro.start();
+                }
+            };
 
-               if(typeof(step)=="number"){
-                   intro.goToStep(step).start();
-               }
-               else{
-                   intro.start();
-               }
-
-           };
-
-
-           if(attrs.ngIntroAutostart == "true"){
-               //Only runs when DOM is ready
-               $timeout(function() {scope[attrs.ngIntroMethod]();}, 0);
-           }
-       }
-   }
+            if(attrs.ngIntroAutostart == 'true') {
+                $timeout(function() {
+                    $parse(attrs.ngIntroMethod)(scope)();
+                });
+            }
+        }
+    }
 }]);
