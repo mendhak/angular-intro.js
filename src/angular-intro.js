c57975fec3340f9cf71b80ab7689092d42495546
Pavan kumar <katakampavan.btech@gmail.com>
2015-09-02 20:05:08 +0530

    working for normal, aswellas for amd also

--- src/angular-intro.js
+++ src/angular-intro.js
@@ -7,7 +7,8 @@
         root.angularIntroJs = factory(root.angular, root.introJs);
     }
 }(this, function (angular, introJs) {
-    introJs=introJs.introJs;
+    if(typeof introJs == 'object')
+        introJs=introJs.introJs;
 
     var ngIntroDirective = angular.module('angular-intro', []);
 
