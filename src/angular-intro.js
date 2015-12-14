82aac8371d0e44b3fd79b21652c6b017cfe4fc17
Michael Asimakopoulos <m.asimakopoulos@gmail.com>
2015-12-14 23:39:48 +0200

    make sure all watches are cleared

--- src/angular-intro.js
+++ src/angular-intro.js
@@ -34,11 +34,13 @@
             link: function(scope, element, attrs) {
 
                 var intro;
+                var refreshWatch;
+                var navigationWatch;
 
                 scope.ngIntroMethod = function(step) {
 
 
-                    var navigationWatch = scope.$on('$locationChangeStart', function(){
+                    navigationWatch = scope.$on('$locationChangeStart', function(){
                       intro.exit();
                     });
 
@@ -52,7 +54,7 @@
                     intro.setOptions(scope.ngIntroOptions);
 
                     if (scope.ngIntroAutorefresh) {
-                      scope.$watch(function(){
+                      refreshWatch = scope.$watch(function(){
                         intro.refresh();
                       });
                     }
@@ -61,7 +63,7 @@
                         intro.oncomplete(function() {
                             scope.ngIntroOncomplete.call(this, scope);
                             $timeout(function() {scope.$digest();});
-                            navigationWatch();
+                            clearWatches();
                         });
                     }
 
@@ -69,7 +71,7 @@
                         intro.onexit(function() {
                             scope.ngIntroOnexit.call(this, scope);
                             $timeout(function() {scope.$digest();});
-                            navigationWatch();
+                            clearWatches();
                         });
                     }
 
@@ -114,7 +116,7 @@
                     callback();
                 };
 
-                var autoStartWatch = scope.$watch('ngIntroAutostart', function (){
+                var autoStartWatch = scope.$watch('ngIntroAutostart', function () {
                     if(scope.ngIntroAutostart){
                         $timeout(function() {
                             scope.ngIntroMethod();
@@ -126,6 +128,15 @@
                 scope.$on('$locationChangeSuccess', function() {
                     if (typeof intro !== 'undefined')
                         intro.exit();
+                });
+
+                var clearWatches = function() {
+                    if(navigationWatch) navigationWatch();
+                    if(refreshWatch) refreshWatch();
+                };
+
+                scope.$on('$destroy', function() {
+                    clearWatches();
                 });
             }
         };
